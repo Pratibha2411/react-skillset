0b21d65ebe1199a1c42abfa0e8cebf90fb690515
Pratibha2411 <pbaloriya@gmail.com>
2023-01-18 20:44:47 +0530

    react-portfolio

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+import project1 from '../src/assets/images/project-1.png'
+import project2 from '../src/assets/images/project-2.png'
+import project3 from '../src/assets/images/project-3.png'
+import project4 from '../src/assets/images/project-4.png'
+import project5 from '../src/assets/images/project-5.png'
+import project7 from '../src/assets/images/project-7.png'
+import project8 from '../src/assets/images/project-8.png'
+import project9 from '../src/assets/images/project-9.png'
+
+const logotext = "TANU";
+const meta = {
+    title: "Pratibha Baloriya",
+    description: "I’m Pratibha Baloriya software developer _ Full stack developer,currently working in India",
+};
+
+const introdata = {
+    title: "I’m Pratibha Baloriya",
+    animated: {
+        first: "I develop websites",
+        second: "I develop web/mobile apps",
+        third: "I love development",
+    },
+    description: "An Enthusiastic Software developer who is giving total support to the organization that I work for with the experience and capability that I have.",
+    your_img_url: "https://ouch-cdn2.icons8.com/aDYEtct3nlD_KQdec9Vc5lpyu_bWtjPxVTNchfDaj64/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDI2/LzQ5M2Y3NWNlLTNj/OGItNDE2My05MDkw/LTJjZjlmYjUyNWRk/OC5zdmc.png",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "An Enthusiastic Software developer who is giving total support to the organization that I work for with the experience and capability that I have.",
+};
+const worktimeline = [{
+        jobtitle: "Jr Software Developer",
+        where: "Bynebits",
+        date: "2021",
+    },
+    {
+        jobtitle: "Software Developer Intern",
+        where: "Freelance",
+        date: "June 2020",
+    },
+    {
+        jobtitle: "Web Developer Intern",
+        where: "Freelance",
+        date: "January 2020",
+    },
+];
+
+const skills = [{
+        name: "Web Development",
+        value: 90,
+    },
+    {
+        name: "UI/UX Developer",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React Native",
+        value: 60,
+    },
+    {
+        name: "ReactJs",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "As a UX/UI Designer, I am a creative and analytical problem-solver who is passionate about creating beautiful and intuitive experiences for users! I have expertise in user research, wireframing, prototyping, and usability testing, and possess the skills and knowledge necessary to design and develop effective and engaging digital products.",
+    },
+    {
+        title: "Web/Mobile Apps",
+        description: "Eager software developer, looking to participate into building something great. I have a strong background on web/mobile app development,I have expertise in ReactJs, JavaScript, Rest APIs, etc & having knowledge of Python, Node, JavaScript, MongoDB, Express, Angular,Firebase, SQL, GraphQL also used Tools & OS - Visual Studio, IntelliJ, Xcode, CS50, Linux many more...",
+    },
+    {
+        title: "Wordpress Design & Plugins",
+        description: "As a wordpress developer I've worked on: Web Development, WordPress Customization, WordPress Theme Development, WordPress Plugin Development, WooCommerce Extensions, WooCommerce Modifications, WooCommerce Enhancements.",
+    },
+];
+
+const dataportfolio = [{
+        img: project1,
+        desctiption: "Portfolio",
+        link: "https://pratibha2411.github.io/Portfolio_responsive/",
+    },
+    {
+        img: project2,
+        desctiption: "Classroom attendance & Administrators' web app",
+        link: "#",
+    },
+    {
+        img: project3,
+        desctiption: "SECURE KEEP NOTE",
+        link: "#",
+    },
+    {
+        img: project4,
+        desctiption: "GYM WEBSITE",
+        link: "#",
+    },
+    {
+        img: project5,
+        desctiption: "Portfolio webApp",
+        link: "#",
+    },
+    {
+        img: project7,
+        desctiption: "WEBSITE",
+        link: "#",
+    },
+
+    {
+        img: project8,
+        desctiption: "Website",
+        link: "#",
+    },
+    {
+        img: project9,
+        desctiption: "Blog Website",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "pratibhabaloriya@gmail.com",
+    YOUR_FONE: "live:.cid.973cf23ffe362f1b",
+    description: "LinkedIn: https://www.linkedin.com/in/pratibha-baloriya-5b36641b7/",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Pratibha2411",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/pratibha-baloriya-5b36641b7/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
